01fd3b1e491ece8663bbc2eb2cdbd427bc49964f
BalkrishanSingh <Balkrishan62005@gmail.com>
2024-10-14 13:10:39 +0530

    Changed coordinate to use record instead

--- FountainOfObjects/Game/Grid/Rooms/Coordinate.cs
+++ FountainOfObjects/Game/Grid/Rooms/Coordinate.cs
@@ -1,29 +1,7 @@
-using System.Data;
-using System.Diagnostics.CodeAnalysis;
-
 namespace FountainOfObjects.Game.Grid.Rooms;
 
-public struct Coordinate
+public record struct Coordinate(int Row, int Column)
 {
-    public int Row{get;}
-    public int Column{get;}
-
-    public Coordinate(int row, int column)
-    {
-        Row = row;
-        Column = column;
-    }
-
-    public static bool operator ==(Coordinate a, Coordinate b)
-    {
-        return  a.Row == b.Row && a.Column == b.Column ;
-    } 
-    
-    public static bool operator !=(Coordinate a, Coordinate b)
-    {
-        return  a.Row != b.Row|| a.Column != b.Column ;
-    }
-
     public bool CheckAdjacency(Coordinate coordinate) => CheckDiagonalAdjacency(coordinate) || CheckOrthogonalAdjacency(coordinate);
     private bool CheckOrthogonalAdjacency(Coordinate coordinate) => Math.Abs(Row - coordinate.Row) + Math.Abs(Column - coordinate.Column) == 1;
     private bool CheckDiagonalAdjacency(Coordinate coordinate) => Math.Abs(Row - coordinate.Row) == 1 && Math.Abs(Column - coordinate.Column) == 1;
