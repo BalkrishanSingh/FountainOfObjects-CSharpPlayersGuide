557eff079980ab6f7e6609b7143d8b24a6a5c8bb
BalkrishanSingh <Balkrishan62005@gmail.com>
2024-10-17 14:53:01 +0530

    Added basic functionality to coordinate class

--- FountainOfObjects/Game/Grid/Rooms/Coordinate.cs
+++ FountainOfObjects/Game/Grid/Rooms/Coordinate.cs
@@ -2,8 +2,21 @@ namespace FountainOfObjects.Game.Grid.Rooms;
 
 public record struct Coordinate(int Row, int Column)
 {
-    public bool CheckAdjacency(Coordinate coordinate) => CheckDiagonalAdjacency(coordinate) || CheckOrthogonalAdjacency(coordinate);
-    private bool CheckOrthogonalAdjacency(Coordinate coordinate) => Math.Abs(Row - coordinate.Row) + Math.Abs(Column - coordinate.Column) == 1;
-    private bool CheckDiagonalAdjacency(Coordinate coordinate) => Math.Abs(Row - coordinate.Row) == 1 && Math.Abs(Column - coordinate.Column) == 1;
-}
+    public bool CheckAdjacency(Coordinate coordinate) =>
+        CheckDiagonalAdjacency(coordinate) || CheckOrthogonalAdjacency(coordinate);
 
+    private bool CheckOrthogonalAdjacency(Coordinate coordinate) =>
+        Math.Abs(Row - coordinate.Row) + Math.Abs(Column - coordinate.Column) == 1;
+
+    private bool CheckDiagonalAdjacency(Coordinate coordinate) =>
+        Math.Abs(Row - coordinate.Row) == 1 && Math.Abs(Column - coordinate.Column) == 1;
+
+    public override string ToString()
+    {
+        return $"(Row: {Row}, Column: {Column})";
+    }
+    
+    public static Coordinate operator +(Coordinate a, Coordinate b) => new(a.Row + b.Row, a.Column + b.Column);
+    public static Coordinate operator -(Coordinate a, Coordinate b) => new(a.Row - b.Row, a.Column - b.Column);
+
+}
